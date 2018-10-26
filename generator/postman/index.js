a20d15eb2a6904c3cad8ac7481fc458f09627644
Alexander Schwartzberg <aeksco@gmail.com>
2018-10-26 16:30:22 -0400

    Implements collection variables for model ids, JSON objects for POST routes

--- generator/postman/index.js
+++ generator/postman/index.js
@@ -1,14 +1,51 @@
 const Generator = require('@codotype/generator')
+const _ = require('lodash')
 
 // // // //
 
 module.exports = class PostmanCollectionJson extends Generator {
   async write ({ blueprint }) {
 
+
+    // Defines a new model object
+    // TODO - abstract this into @codotype/util
+    function buildModelObject (schema) {
+      let newModel = {}
+
+      schema.attributes.forEach((attr) => {
+        if (attr.datatype === 'STRING_ARRAY') {
+          newModel[attr.identifier] = []
+        } else if (attr.datatype === 'NUMBER') {
+          newModel[attr.identifier] = 0
+        } else if (attr.datatype === 'JSON') {
+          newModel[attr.identifier] = {}
+        } else if (attr.datatype === 'BOOL') {
+          newModel[attr.identifier] = attr.default_value || true
+        } else if (attr.datatype === 'TEXT') {
+          newModel[attr.identifier] = attr.default_value || `${attr.label} Value`
+        }
+      })
+
+      schema.relations.forEach((rel) => {
+         if (rel.type === 'HAS_MANY') {
+           newModel[rel.alias.identifier + '_ids'] = []
+         } else if (['BELONGS_TO', 'HAS_ONE'].includes(rel.type)) {
+           newModel[rel.alias.identifier + '_id'] = ''
+         }
+      })
+
+      // Stringifies newModel JSON
+      const stringified = JSON.stringify(newModel, null, 2)
+
+      // Returns the escapsed version for the postman_collection.json file
+      return stringified.replace(/\n/g, "\\n").replace(/\"/g, '\\"')
+    }
+
     // Copies models
     await this.copyTemplate(
       this.templatePath('postman-collection.json'),
-      this.destinationPath(blueprint.identifier + '.postman_collection.json')
+      this.destinationPath(blueprint.identifier + '.postman_collection.json'),
+      { buildModelObject }
     )
 
   }
